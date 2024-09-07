494d7d1fad5206fe9d7bbdc9c13a86ce54dd7db8
Sandro Mirianashvili <mirianashvili.sandro@gmail.com>
2024-09-07 18:10:27 +0400

    add lecture 01 02

---
+++ Lecture01/main01.py
@@ -0,0 +1,24 @@
+# Python 1st Lecture
+
+"""
+Author: John Doe
+Language: Python
+Date: 03/09/2024
+"""
+
+#print("Hello Python!!!")
+#print("Mziuri Course")
+#print('Python / Flask')
+
+
+"""
+კომენარი დაამატეთ
+სახელი გვარი
+სკოლა / უნივერსიტეტი / სამსახური
+პროგრამირების ენა
+"""
+
+# my first assignment
+print("John Doe")
+print("XYZ Corp")
+print("Python, Java")
---
+++ Lecture02/main01.py
@@ -0,0 +1,58 @@
+"""
+print(2 + 3)
+print(10 - 2)
+print(3 * 4)
+print(10 / 2)
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+print(10 ** 4)
+
+print(2 * (5 + 12))
+print(2 * (3 + 5) / 10)
+"""
+
+#print((3 + (11 + 9)**4 + (2 + 4) / 5)/(4 + 12 * (10 + 7) ** 2))
+
+"""
+a = 10
+b = 24
+
+print(a)
+print(b)
+print(a + b)
+
+r = a + b
+
+print(r)
+"""
+
+"""
+შექმენით ორი ცვლადი x,y 
+მიანიჭეთ მათ მნიშვნელობები
+დაბეჭდეთ ეკრანზე ჯამი, სხვაობა, ნამრავლი, გაყოფა და x ხარისხად y
+"""
+
+"""
+x = 10
+y = 3
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x ** y)
+"""
+
+x = 10
+print(x, type(x))
+
+y = 1.5
+print(y, type(y))
+
+s = "welcome"
+print(s, type(s))
+
+# True False
+b = False
+print(b, type(b))
